53e8885f9f6a7cf50f15ce0ca70d2e008dd6be61
jaanu-setti <jaanusetti03@gmail.com>
2024-10-04 19:53:34 +0530

    signup route is connected to backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,25 +1,42 @@
 const express = require ('express');
 const route = express.Router();
 const User = require('../models/user')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validate = require('validator');
 route.post('/signup' , async(req,res)=>{
   try{
     const {name , email , phone , state , district , password , confirmpassword}=req.body;
-    if(name===''||email===''||phone===''||state===''||district===''||password===''||confirmpassword===''){
-        res.status(401).json({message : 'some of the required fields are empty'})
+    console.log(req.body)
+    if (!name || !email || !phone || !state || !district || !password || !confirmpassword) {
+        console.log('Some of the required fields are empty' )
+        return res.status(401).json({ message: 'Some of the required fields are empty' });
+      }
+    const existinguser = await User.findOne({email});
+    if(existinguser){
+       return res.status(403).json({message : 'user already esists with given email id'})
     }
+    if(!validate.isEmail(email)){
+        return  res.status(400).json({message : "email should be valid "})
+    }
+    if(password!==confirmpassword){
+        return  res.status(400).json({message : "password and confirm password should be same"})
+    }
+    const hashedpassword =await bcrypt.hash(password,10)
     const userdetails = await User.create({
       name,
       email,
       phone,
       state,
       district,
-      password,
-      confirmpassword
+      password : hashedpassword,
+      confirmpassword 
     })
-    res.status(201).json({message : 'user data stored successfully' , data : userdetails})
+    console.log('data : ' ,userdetails)
+    return res.status(201).json({message : 'user data stored successfully' , data : userdetails})
 
   }catch(err){
-    res.status(500).json({message : err.message})
+    return res.status(500).json({message : err.message})
   }
 })
 module.exports = route;
